b97d4c55348235dac41ca2313d37d614c4f696e8
Kevin <hihellohi610@gmail.com>
2017-10-07 00:49:26 +1100

    admin stuff

--- implementation.py
+++ implementation.py
@@ -3,23 +3,9 @@ import numpy as np
 import glob #this will be useful when reading reviews from file
 import os
 import tarfile
-import string.punctuation
+import string
 
 batch_size = 50
-
-def check_file(filename, expected_bytes):
-    """Download a file if not present, and make sure it's the right size."""
-    if not os.path.exists(filename):
-        print("please make sure {0} exists in the current directory".format(filename))
-    statinfo = os.stat(filename)
-    if statinfo.st_size == expected_bytes:
-        print('Found and verified', filename)
-    else:
-        print(statinfo.st_size)
-        raise Exception(
-            "File {0} didn't have the expected size. Please ensure you have downloaded the assignment files correctly".format(filename))
-    return filename
-
 
 # Read the data into a list of strings.
 def extract_data(filename):
@@ -31,69 +17,23 @@ def extract_data(filename):
     return
 
 def read_data():
-    print("READING DATA")
     data = []
     dir = os.path.dirname(__file__)
     file_list = glob.glob(os.path.join(dir,
                                         'data2/pos/*'))
     file_list.extend(glob.glob(os.path.join(dir,
                                         'data2/neg/*')))
-    print("Parsing %s files" % len(file_list))
     for f in file_list:
-        with open(f, "r") as openf:
+        with open(f, "r", encoding="utf-8") as openf:
             try:
                 s = openf.read()
             except:
                 print(f);
                 raise;
             no_punct = ''.join(c for c in s if c not in string.punctuation)
-            data.extend(no_punct.split())
+            data.append(no_punct.split())
 
-    print(data[:5])
     return data
-
-def build_dataset(words, n_words):
-    """Process raw inputs into a dataset."""
-    count = [['UNK', -1]]
-    count.extend(collections.Counter(words).most_common(n_words - 1))
-    dictionary = dict()
-    for word, _ in count:
-        dictionary[word] = len(dictionary)
-    data = list()
-    unk_count = 0
-    for word in words:
-        if word in dictionary:
-            index = dictionary[word]
-        else:
-            index = 0  # dictionary['UNK']
-            unk_count += 1
-        data.append(index)
-    count[0][1] = unk_count
-    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-    return data, count, dictionary, reversed_dictionary
-
-def get_dataset(vocabulary_size):
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "data.npy")):
-        print("loading saved parsed data, to reparse, delete 'data.npy'")
-        data = np.load("data.npy")
-        count = np.load("count.npy")
-        dictionary = np.load("Word2Idx.npy").item()
-        reverse_dictionary = np.load("Idx2Word.npy").item()
-    else:
-        filename = check_file('reviews.tar.gz', 14839260)
-        extract_data(filename) # unzip
-        vocabulary = read_data()
-        print('Data size', len(vocabulary))
-        # Step 2: Build the dictionary and replace rare words with UNK token.
-        data, count, dictionary, reverse_dictionary =\
-            build_dataset(vocabulary, vocabulary_size)
-
-        np.save("data", data)
-        np.save("count", count)
-        np.save("Idx2Word", reverse_dictionary)
-        np.save("Word2Idx", dictionary)
-        del vocabulary  # Hint to reduce memory.
-    return data, count, dictionary, reverse_dictionary
 
 def load_data(glove_dict):
     """
@@ -103,7 +43,26 @@ def load_data(glove_dict):
     reviews should be the negative reviews.
     RETURN: numpy array of data with each row being a review in vectorized
     form"""
-	data, count, dictionary, reverse_dictionary = get_dataset(vocabulary_size)
+    extract_data('reviews.tar.gz') # unzip
+    vocabulary = read_data()
+    data = np.zeros((len(vocabulary), 40))
+
+    #do things to data here
+
+    for i, review in enumerate(vocabulary):
+        j = 0;
+        for word in review:
+            index = glove_dict.get(word, None);
+            if index == None:
+                continue;
+
+            data[i][j] = index;
+            j += 1;
+
+            if j >= 40:
+                break
+
+    del vocabulary  # Hint to reduce memory.
     return data
 
 
@@ -116,14 +75,17 @@ def load_glove_embeddings():
             word_index_dict: a dictionary matching a word in string form to
             its index in the embeddings array. e.g. {"apple": 119"}
     """
-    #data = open("glove.6B.50d.txt",'r',encoding="utf-8")
-    #if you are running on the CSE machines, you can load the glove data from here
-    data = open("/home/cs9444/public_html/17s2/hw2/glove.6B.50d.txt",'r',encoding="utf-8")
-	translator = str.maketrans('','',string.punctuation);
-	for line in data:
-		words = line.lower().translate(translator).split();
+    word_index_dict = {'UNK' : 0};
+    embeddings = [[0] * 50];
 
-	data.close();
+    with open("glove.6B.50d.txt",'r',encoding="utf-8") as data:
+        #if you are running on the CSE machines, you can load the glove data from here
+        #data = open("/home/cs9444/public_html/17s2/hw2/glove.6B.50d.txt",'r',encoding="utf-8")
+        for line in data:
+            words = line.split();
+            word_index_dict[words[0]] = len(embeddings);
+            embeddings.append([float(i) for i in words[1:]]);
+
     return embeddings, word_index_dict
 
 
